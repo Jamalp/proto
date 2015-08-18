6b20544bdad8f6c7f55e1b836f2c77956e7b0361
Jamal Powell <jamalkpowell@gmail.com>
2015-08-18 11:55:05 -0400

    content page

--- scripts/main.js
+++ scripts/main.js
@@ -15,10 +15,31 @@ var app = app || {};
 			});
 		},
 
+		fadeHero : function() {
+			function fader() {
+				  var r = $('.blurred'),
+				    wh = $(window).height(),
+				    dt = $(document).scrollTop(),
+				    elView, opacity;
+
+				  // Loop elements with class "blurred"
+				  r.each(function() {
+				    elView = wh - ($(this).offset().top - dt + 200);
+				    if (elView > 0) { // Top of DIV above bottom of window.
+				      opacity = 1 / (wh + $(this).height()) * elView * 2
+				      if (opacity < 1) // Bottom of DIV below top of window.
+				        $(this).css('opacity', opacity);
+				    }
+				  });
+				}
+
+				$(document).bind('scroll', fader);
+		},
+
 
 		init : function() {
-			console.log("online");
 			app.main.carousel();
+			app.main.fadeHero();
 		}
 	}
 
